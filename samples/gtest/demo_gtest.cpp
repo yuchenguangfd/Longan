e801e000a966baa5ca76d7c93ddc9f4b86709a44
ChenguangYu <yuchenguangfd@gmail.com>
2014-07-20 23:52:35 +0800

    添加了gtest单元测试框架

---
+++ samples/gtest/demo_gtest.cpp
@@ -0,0 +1,27 @@
+/*
+ * helloworld.cpp
+ * Created on: Jul 20, 2014
+ * Author: chenguangyu
+ * Show basic usage of gtest library
+ */
+
+#include <gtest/gtest.h>
+
+int GCD(int a, int b) {
+    if (a == 0 || b == 0) {
+        throw "don't do that";
+    }
+    int c = a % b;
+    if (c == 0) return b;
+    return GCD(b, c);
+}
+
+TEST(GCDTest, HandleNoneZeroInput) {
+    EXPECT_EQ(2, GCD(4, 10));
+    EXPECT_EQ(6, GCD(30, 18));
+}
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
